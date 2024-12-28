c522b5171a47d67c0f65f7b9b7e39816491d0f62
nidenseek <nidenseekn@vk.com>
2024-12-28 16:41:29 +0300

    Update show.js

--- Home/UI/JS/show.js
+++ Home/UI/JS/show.js
@@ -1,19 +1,16 @@
 const snowContainer = document.getElementById('snow-container');
 
-    function createSnowflake() {
-        const snowflake = document.createElement('div');
-        snowflake.classList.add('snowflake');
-        snowflake.textContent = '❄';
+function createSnowflake() {
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    snowflake.textContent = '❄'; 
+    snowflake.style.left = `${Math.random() * 100}vw`;
+    snowflake.style.animationDuration = `${Math.random() * 5 + 2}s`;
+    snowflake.style.fontSize = `${Math.random() * 10 + 10}px`;
+    snowContainer.appendChild(snowflake);
+    setTimeout(() => {
+        snowflake.remove();
+    }, 5000);
+}
 
-        snowflake.style.left = Math.random() * 100 + 'vw';
-        snowflake.style.fontSize = Math.random() * 10 + 10 + 'px'; 
-        snowflake.style.animationDuration = Math.random() * 1 + 1 + 's';
-        snowflake.style.animationDelay = Math.random() * 1 + 's';
-
-        snowContainer.appendChild(snowflake);
-        setTimeout(() => {
-            snowflake.remove();
-        }, 2000);
-    }
-
-    setInterval(createSnowflake, 50);
+setInterval(createSnowflake, 50);
